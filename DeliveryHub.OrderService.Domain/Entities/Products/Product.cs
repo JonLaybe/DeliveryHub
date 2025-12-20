c5860c92b849c21d86119e9d3ecf86650cd28d6a
GlebS <70696085+JonLaybe@users.noreply.github.com>
2025-12-21 01:18:21 +0700

    Created models for order service

---
+++ DeliveryHub.OrderService.Domain/Entities/Products/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace DeliveryHub.OrderService.Domain.Entities.Products
+{
+    public class Product : BaseEntity
+    {
+        /// <summary>
+        /// External product ID.
+        /// </summary>
+        public Guid ArticleNumber { get; set; }
+
+        /// <summary>
+        ///  Name product.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        ///  Price product.
+        /// </summary>
+        public int Price { get; set; } = 0;
+
+        /// <summary>
+        ///  Photo preview Url.
+        /// </summary>
+        public string PhotoPreviewUrl { get; set; }
+    }
+}
