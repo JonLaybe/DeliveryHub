7b58d9be8e572de5e91df7a049bf1895fe4b6e77
lasm16 <lasm16@yandex.ru>
2025-12-21 17:34:41 +0300

    Add DAL

---
+++ Services/ChatService/ChatService.DAL/Models/Conversation.cs
@@ -0,0 +1,20 @@
+﻿using ChatService.DAL.Enums;
+
+namespace ChatService.DAL.Models
+{
+    public class Conversation
+    {
+        public Guid Id { get; set; }
+
+        public Guid ProductId { get; set; }
+        public Guid SellerId { get; set; }
+        public Guid BuyerId { get; set; }
+
+        public ConversationStatus Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastMessageAt { get; set; }
+
+        public ICollection<Message> Messages { get; set; }
+    }
+}
---
+++ Services/ChatService/ChatService.DAL/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using ChatService.DAL.Enums;
+
+namespace ChatService.DAL.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+
+        public Guid ConversationId { get; set; }
+        public Conversation Conversation { get; set; }
+
+        public Guid SenderId { get; set; }
+        public SenderRole SenderRole { get; set; }
+
+        public string Text { get; set; }
+        public bool IsRead { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
