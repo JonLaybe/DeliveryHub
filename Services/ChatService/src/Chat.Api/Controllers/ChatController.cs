c55bcfc4a54b9599c76e8e4304435af030f77a39
lasm16 <lasm16@yandex.ru>
2025-12-23 23:39:24 +0300

    Add ChatService solution

---
+++ Services/ChatService/src/Chat.Api/Controllers/ChatController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chat.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChatController : ControllerBase
+    {
+    }
+}
