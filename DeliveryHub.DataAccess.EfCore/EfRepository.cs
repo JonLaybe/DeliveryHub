a5cfc7ef0c73b5a55808ddc662e369907e9fc548
Lebedev <lebedev.p@mjr.ru>
2026-01-17 18:42:22 +0300

    Добавление абстрактного репозитория и реализации под EF Core

---
+++ DeliveryHub.DataAccess.EfCore/EfRepository.cs
@@ -0,0 +1,53 @@
+﻿using DeliveryHub.Abstractions.DataAccess;
+using DeliveryHub.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryHub.DataAccess.EfCore
+{
+    public class EfRepository<T, K>
+        : IRepository<T, K> where T : BaseEntity<K> where K : struct, IEquatable<K>
+    {
+        private readonly DbContext _context;
+
+        public EfRepository(DbContext context) { _context = context; }
+
+        public async Task<K> CreateAsync(T entity)
+        {
+            await _context.AddAsync(entity);
+
+            await _context.SaveChangesAsync();
+
+            return entity.Id;
+        }
+
+        public async Task DeleteAsync(K id)
+        {
+            var entity = await GetByIdAsync(id);
+
+            if (entity != null)
+            {
+                _context.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public IQueryable<T> GetAll() => _context.Set<T>();
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(K id)
+        {
+            return await _context.FindAsync<T>(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Update(entity);
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Shared/DeliveryHub.Abstractions/DataAccess/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using DeliveryHub.Domain.Entities;
+
+namespace DeliveryHub.Abstractions.DataAccess
+{
+    public interface IRepository<T, K>
+        where T : BaseEntity<K> where K : struct, IEquatable<K>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        IQueryable<T> GetAll();
+
+        Task<T> GetByIdAsync(K id);
+
+        Task DeleteAsync(K id);
+
+        Task<K> CreateAsync(T entity);
+
+        Task UpdateAsync(T entity);
+    }
+}
