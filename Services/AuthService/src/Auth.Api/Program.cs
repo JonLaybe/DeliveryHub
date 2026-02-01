be6c5a7c8095ac16727951d2469f3cca37aa2758
vgorb <git.vgorbachuk@gmail.com>
2026-02-02 01:48:08 +0300

    Подключение DbContext через DI

--- Services/AuthService/src/Auth.Api/Program.cs
+++ Services/AuthService/src/Auth.Api/Program.cs
@@ -1,8 +1,15 @@
+using Auth.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AuthDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"))
+       .UseSnakeCaseNamingConvention());
 
 var app = builder.Build();
 
