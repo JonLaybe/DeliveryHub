7a301cc31eeb6ce14c711bc8b87da334c3f54373
vgorb <git.vgorbachuk@gmail.com>
2026-02-20 23:25:07 +0300

    DTO для Users и ролей

---
+++ Services/AuthService/src/Auth.Api/Contracts/Users/AssignRoleRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Auth.Api.Contracts.Users;
+
+public sealed record AssignRoleRequest(string RoleName);
---
+++ Services/AuthService/src/Auth.Api/Contracts/Users/LockUserRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace Auth.Api.Contracts.Users;
+
+public sealed record LockUserRequest(int? Minutes); // null => lock indefinitely
