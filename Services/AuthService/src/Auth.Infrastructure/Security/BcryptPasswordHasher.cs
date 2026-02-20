ef9b2a32044903a23db307e0d606f78f4c9787ad
vgorb <git.vgorbachuk@gmail.com>
2026-02-21 00:25:27 +0300

    Fix BCrypt

--- Services/AuthService/src/Auth.Infrastructure/Security/BcryptPasswordHasher.cs
+++ Services/AuthService/src/Auth.Infrastructure/Security/BcryptPasswordHasher.cs
@@ -1,10 +1,9 @@
 ﻿using Auth.Application.Abstractions.Security;
-using BCrypt.Net;
 
 namespace Auth.Infrastructure.Security;
 
 public sealed class BcryptPasswordHasher : IPasswordHasher
 {
-    public string Hash(string password) => BCrypt.HashPassword(password);
-    public bool Verify(string password, string passwordHash) => BCrypt.Verify(password, passwordHash);
+    public string Hash(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+    public bool Verify(string password, string passwordHash) => BCrypt.Net.BCrypt.Verify(password, passwordHash);
 }
