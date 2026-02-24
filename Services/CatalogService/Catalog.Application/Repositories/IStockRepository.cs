92b0e811c72eebe87956abc3ef7f7852b8b354c1
Lebedev <lebedev.p@mjr.ru>
2026-02-25 00:46:21 +0300

    Catalog. Сидирование данных

---
+++ Services/CatalogService/Catalog.Application/Repositories/IStockRepository.cs
@@ -0,0 +1,9 @@
+﻿using DeliveryHub.Catalog.Domain.Entities;
+using Shared.Abstractions.DataAccess;
+
+namespace Catalog.Application.Repositories
+{
+    public interface IStockRepository: IRepository<Stock, Guid>
+    {
+    }
+}
---
+++ Services/CatalogService/Catalog.Domain/Entities/Stock.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Domain.Entities;
+
+namespace DeliveryHub.Catalog.Domain.Entities
+{
+    public class Stock: BaseEntity<Guid>
+    {
+        public Guid ProductId { get; set; }
+        public int TotalQty { get; set; }
+        public int ReservedQty { get; set; }
+
+        public int AvailableQty => TotalQty - ReservedQty;
+    }
+
+}
