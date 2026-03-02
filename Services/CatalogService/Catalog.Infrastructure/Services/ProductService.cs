1d22b3dd20cc14d87907cbf8ed0b9aa8a0fa3706
Lebedev <lebedev.p@mjr.ru>
2026-03-02 23:13:51 +0300

    API. Added GetAll product method

--- Services/CatalogService/Catalog.Infrastructure/Services/ProductService.cs
+++ Services/CatalogService/Catalog.Infrastructure/Services/ProductService.cs
@@ -20,6 +20,47 @@ namespace DeliveryHub.Catalog.Infrastructure.Services
             _stockRepository = stockRepository;
         }
 
+        public async Task<List<ProductDto>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.GetAllAsync(cancellationToken);
+
+            var productIds = products.Select(s => s.Id);
+
+            var stocks = await _stockRepository.GetAll()
+                .Where(x => productIds.Contains(x.ProductId))
+                .ToListAsync();
+
+            var images = await _productImageRepository.GetAll()
+                .Where(x => productIds.Contains(x.ProductId))
+                .ToListAsync();
+
+            var stocksDict = stocks.ToDictionary(k => k.ProductId, v => v);
+
+            var imagesDict = images.ToList()
+                .GroupBy(x => x.ProductId)
+                .ToDictionary(
+                    k => k.Key,
+                    v => v.Select(s => new ProductImageDto
+                    {
+                        ProductId = s.ProductId,
+                        Url = s.Url,
+                        Order = s.Order,
+                        Type = s.Type,
+                    }).ToList());
+
+            return products.Select(p => new ProductDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price,
+                CategoryId = p.CategoryId,
+                Attributes = p.Attributes,
+                AvailableQty = stocksDict.GetValueOrDefault(p.Id)?.AvailableQty ?? 0,
+                Images = imagesDict.GetValueOrDefault(p.Id) ?? []
+            }).ToList();
+        }
+
         public async Task<ProductDto> GetByIdAsync(Guid id, CancellationToken cancellationToken)
         {
             var product = await _productRepository.GetByIdAsync(id, cancellationToken);
