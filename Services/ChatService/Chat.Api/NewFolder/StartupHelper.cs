5e37efa08e20c0d009d43aa277079e3d1a276a72
Alexander Lyubimov <lasm16@yandex.ru>
2026-05-04 14:01:07 +0300

    refactoring (#145)

---
+++ Services/ChatService/Chat.Api/NewFolder/StartupHelper.cs
@@ -0,0 +1,62 @@
+﻿using Chat.Api.Hubs;
+using Chat.Infrastructure.Persistence;
+using Chat.Infrastructure.Seed;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+namespace Chat.Api.NewFolder
+{
+    public static class StartupHelper
+    {
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+            services.AddSignalR();
+            services.AddOpenApi();
+
+            services.AddChatServices();
+
+            var connection = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ChatDbContext>(options =>
+                options.UseNpgsql(connection));
+        }
+
+        public static async Task ApplyMigrationsAndSeedAsync(WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ChatDbContext>();
+
+             await context.Database.MigrateAsync();
+
+            if (app.Environment.IsDevelopment())
+            {
+                await ChatDbSeeder.SeedAsync(context);
+            }
+        }
+
+        public static void ConfigureMiddleware(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.MapScalarApiReference(options =>
+                {
+                    options
+                        .WithTitle("ChatService API")
+                        .WithTheme(ScalarTheme.BluePlanet);
+                });
+            }
+
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+
+            app.MapHub<ChatHub>("/chat");
+            app.MapControllers();
+        }
+    }
+}
