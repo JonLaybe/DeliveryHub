32657ed33d7ee06d295408bd45ebe74bdc41e57b
lasm16 <lasm16@yandex.ru>
2026-05-04 13:59:43 +0300

    refactoring

---
+++ Services/ChatService/Chat.Api/NewFolder/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Chat.Application.Interfaces;
+using Chat.Application.Services;
+using Chat.Infrastructure.Repositories;
+
+namespace Chat.Api.NewFolder
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddChatServices(this IServiceCollection services)
+        {
+            services.AddScoped<IConversationRepository, ConversationRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
+
+            services.AddScoped<IConversationService, ConversationService>();
+            services.AddScoped<IMessageService, MessageService>();
+
+            return services;
+        }
+    }
+}
