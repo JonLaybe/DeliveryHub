5e1de94beeb39b24868c00665ed34b007f43c259
lasm16 <lasm16@yandex.ru>
2026-05-16 02:11:54 +0300

    add service and dbContext

---
+++ Services/ProfileService/Profile.Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using Profile.Domain.Enums;
+
+namespace Profile.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public decimal TotalAmount { get; set; }
+        public List<Product> Products { get; set; } = [];
+    }
+}
