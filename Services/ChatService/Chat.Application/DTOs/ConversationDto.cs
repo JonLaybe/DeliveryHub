fa2e5ebf5fbe30034afcb498f3cb43488c94bb3d
lasm16 <lasm16@yandex.ru>
2026-05-20 16:54:05 +0300

    fix

--- Services/ChatService/Chat.Application/DTOs/ConversationDto.cs
+++ Services/ChatService/Chat.Application/DTOs/ConversationDto.cs
@@ -3,7 +3,7 @@
     public class ConversationDto
     {
         public Guid Id { get; set; }
-		public Guid ProductId { get; set; }
+        public Guid ProductId { get; set; }
         public Guid BuyerId { get; set; }
         public Guid SellerId { get; set; }
         public string Status { get; set; }
