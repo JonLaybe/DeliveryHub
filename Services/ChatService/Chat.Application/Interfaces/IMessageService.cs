5c8842801a80d126150d4988a82501e5d1df846e
lasm16 <lasm16@yandex.ru>
2026-05-26 12:48:25 +0300

    add some changes

--- Services/ChatService/Chat.Application/Interfaces/IMessageService.cs
+++ Services/ChatService/Chat.Application/Interfaces/IMessageService.cs
@@ -4,9 +4,8 @@ namespace Chat.Application.Interfaces
 {
     public interface IMessageService
     {
-        Task<Guid> SendMessageAsync(Guid conversationId, Guid senderId, string text);
-        Task<IReadOnlyList<MessageResponse>> GetMessagesAsync(Guid conversationId);
-        Task MarkMessagesAsReadAsync(Guid conversationId, Guid userId);
+        Task<Guid> SendMessageAsync(Guid conversationId, Guid userId, string text);
+        Task<IReadOnlyList<MessageResponse>> GetMessagesAsync(Guid conversationId, Guid userId);
         Task<Dictionary<Guid, (int unreadCount, string lastMessage)>> GetConversationStatsAsync(IEnumerable<Guid> conversationIds, Guid userId);
     }
 }
--- Services/ChatService/Chat.Application/Services/MessageService.cs
+++ Services/ChatService/Chat.Application/Services/MessageService.cs
@@ -3,6 +3,7 @@ using Chat.Application.Helpers;
 using Chat.Application.Interfaces;
 using Chat.Domain.Entities;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace Chat.Application.Services
 {
@@ -12,20 +13,23 @@ namespace Chat.Application.Services
         private readonly IConversationRepository _conversationRepository;
         private readonly IOnlineStatusService _onlineStatusService;
         private readonly ILogger<MessageService> _logger;
+        private readonly IDatabase _dbRedis;
 
         public MessageService(
             IMessageRepository messageRepository,
             IConversationRepository conversationRepository,
             IOnlineStatusService onlineStatusService,
-            ILogger<MessageService> logger)
+            ILogger<MessageService> logger,
+            IConnectionMultiplexer redis)
         {
             _messageRepository = messageRepository;
             _conversationRepository = conversationRepository;
             _onlineStatusService = onlineStatusService;
             _logger = logger;
+            _dbRedis = redis.GetDatabase();
         }
 
-        public async Task<Guid> SendMessageAsync(Guid conversationId, Guid senderId, string text)
+        public async Task<Guid> SendMessageAsync(Guid conversationId, Guid userId, string text)
         {
             if (string.IsNullOrWhiteSpace(text))
                 throw new ArgumentException("Message text cannot be empty");
@@ -39,14 +43,15 @@ namespace Chat.Application.Services
             {
                 Id = Guid.NewGuid(),
                 ConversationId = conversationId,
-                SenderId = senderId,
+                SenderId = userId,
+                SenderRole = Domain.Enums.SenderRole.User,
                 Text = text,
                 IsRead = false,
                 CreatedAt = DateTime.UtcNow
             };
 
             await _messageRepository.AddAsync(message);
-            await _onlineStatusService.SetOnlineAsync(senderId);
+            await _onlineStatusService.SetOnlineAsync(userId);
 
             conversation.LastMessageAt = DateTime.UtcNow;
 
@@ -54,24 +59,22 @@ namespace Chat.Application.Services
             await _conversationRepository.SaveChangesAsync();
 
             _logger.LogInformation(
-                "Message saved. Id={Id}, ConversationId={ConversationId}, SenderId={SenderId}",
+                "Message saved. Id={Id}, ConversationId={ConversationId}, SenderId={SenderId}, Text={Text}",
                 message.Id,
                 message.ConversationId,
-                message.SenderId);
+                message.SenderId,
+                message.Text);
 
             return message.Id;
         }
 
-        public async Task<IReadOnlyList<MessageResponse>> GetMessagesAsync(Guid conversationId)
+        public async Task<IReadOnlyList<MessageResponse>> GetMessagesAsync(Guid conversationId, Guid userId)
         {
+            //var cached = GetMessagesFromCachedAsync(conversationId);
             var messages = await _messageRepository.GetMessagesByConversationIdAsync(conversationId);
+            await _messageRepository.SetMessageIsReadTrueAsync(conversationId, userId);
             var response = Mapper.GetMessageResponseList(messages);
             return response;
-        }
-
-        public async Task MarkMessagesAsReadAsync(Guid conversationId, Guid userId)
-        {
-            await _messageRepository.SetMessageIsReadTrueAsync(conversationId, userId);
         }
 
         public async Task<Dictionary<Guid, (int unreadCount, string lastMessage)>> GetConversationStatsAsync(IEnumerable<Guid> conversationIds, Guid userId)
@@ -91,5 +94,22 @@ namespace Chat.Application.Services
 
             return grouped;
         }
+
+        //public async Task SetOnlineAsync(Guid userId)
+        //{
+        //    var key = GetKey(userId);
+        //    await _db.StringSetAsync(key, true, TimeSpan.FromSeconds(OnlineTtlSeconds));
+        //}
+
+        //public async Task SetOfflineAsync(Guid userId)
+        //{
+        //    var key = GetKey(userId);
+        //    await _db.KeyDeleteAsync(key);
+        //}
+
+        //private async Task<bool> GetMessagesFromCachedAsync(Guid conversationId)
+        //{            
+        //    return await _dbRedis.StringGetAsync(conversationId.ToString());
+        //}
     }
 }
