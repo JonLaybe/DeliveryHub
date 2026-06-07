15e5802228a147803838e3f3f963f3efa046b1df
vgorb <git.vgorbachuk@gmail.com>
2026-06-07 18:34:48 +0300

    Added endpoint for editing profile

---
+++ Services/AuthService/src/Auth.Api/Contracts/Profile/UpdateMyProfileRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Auth.Api.Contracts.Profile;
+
+public sealed record UpdateMyProfileRequest(
+    string? FirstName,
+    string? LastName,
+    string? PhotoUrl,
+    DateOnly? BirthDate,
+    string? PhoneNumber,
+    string? Country,
+    string? City
+);
